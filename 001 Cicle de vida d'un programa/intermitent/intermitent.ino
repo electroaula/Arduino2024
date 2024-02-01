8905c9b6fdfc61608247ed5aa18c71ba7a4dd658
Electroaula <igonza28@xtec.cat>
2024-02-01 13:27:14 +0100

    sr

--- 001 Cicle de vida d'un programa/intermitent/intermitent.ino
+++ 001 Cicle de vida d'un programa/intermitent/intermitent.ino
@@ -1,37 +1,48 @@
-/*
-  Blink
+int T  =  9;
+int Y  =  4;
+int K  =  8;
+int N  =  3;
+int P  = - 3;
+boolean Z  = true;
+boolean W = false;
+boolean A  =  false;
+boolean B  =  true;
 
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  delay(2000);
+  Serial.println("a. Afirmacio 1: (T > 10 && T < 14) = ");
+
+  if (T > 10 && T < 14)
+  {
+    Serial.print("True");
+  }
+  else
+  {
+    Serial.print("False");
+  }
+              
+  Serial.println("b. Afirmacio 2: (Y > 4 || Y < 7)=");
+          if ((Y > 4 || Y < 7)); // Afirmacio 2
+              Serial.print("boolean A" or "boolean B");
+  Serial.println()
+  if ((K > 8 && K < 10)); // Afirmacio 3
+  Serial.print("boolean A" or "boolean B");
+  if ((N > 2 || N < 20)); // Afirmacio 4
+  Serial.print("boolean A" or "boolean B");
+  if ((P > 0 && P < -3)); // Afirmacio 5
+  Serial.print("boolean A" or "boolean B");
+  if (T > 0 || Z); // Afirmacio 6
+  Serial.print("boolean A" or "boolean B");
+  if (P > 0 && B); // Afirmacio 7
+  Serial.print("boolean A" or "boolean B");
+  if (W && B); // Afirmacio 8
+  Serial.print("boolean A" or "boolean B");
+  if (W && A); // Afirmacio 9
+  Serial.print("boolean A" or "boolean B");
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  // put your main code here, to run repeatedly:
+
 }
